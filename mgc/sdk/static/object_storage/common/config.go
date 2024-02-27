09c790ad0422abe32a37cfe359ccd7e85767888c
Gefferson Ferraz <me@gefferson.com.br>
2024-02-27 10:52:03 -0300

    feat: rename regions

--- mgc/sdk/static/object_storage/common/config.go
+++ mgc/sdk/static/object_storage/common/config.go
@@ -5,15 +5,15 @@ import "magalu.cloud/core/config"
 type Config struct {
 	Workers   int    `json:"workers,omitempty" jsonschema:"description=Number of routines that spawn to do parallel operations within object_storage,default=5,minimum=1"`
 	ChunkSize uint64 `json:"chunkSize,omitempty" jsonschema:"description=Chunk size to consider when doing multipart requests. Specified in Mb,default=5,minimum=5,maximum=5120"`
-	Region    string `json:"region,omitempty" jsonschema:"description=Region to reach the service,default=br-ne1,enum=br-ne-1,enum=br-se-1,enum=br-mgl-1"`
+	Region    string `json:"region,omitempty" jsonschema:"description=Region to reach the service,default=br-ne1,enum=br-ne1,enum=br-se1,enum=br-mgl1"`
 	// See more about the 'squash' directive here: https://pkg.go.dev/github.com/mitchellh/mapstructure#hdr-Embedded_Structs_and_Squashing
 	config.NetworkConfig `json:",squash"` // nolint
 }
 
 var regionMap = map[string]string{
-	"br-ne-1":  "br-ne-1",
-	"br-se-1":  "br-se-1",
-	"br-mgl-1": "br-se-1",
+	"br-ne1":  "br-ne-1",
+	"br-se1":  "br-se1",
+	"br-mgl1": "br-se-1",
 }
 
 func (c *Config) translateRegion() string {
