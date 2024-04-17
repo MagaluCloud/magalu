89dbb316651db6e961d7c3a7f9fad77236ad654c
Gefferson Ferraz <me@gefferson.com.br>
2024-04-17 14:31:43 -0300

    fix: change http retry (#1005)

--- mgc/core/http/http_retry.go
+++ mgc/core/http/http_retry.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"os"
 	"time"
@@ -28,12 +30,34 @@ func NewClientRetryerWithAttempts(transport http.RoundTripper, attempts int) *Cl
 	}
 }
 
+func (r *ClientRetryer) cloneRequestBody(req *http.Request) (*bytes.Buffer, error) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	req.Body = io.NopCloser(bytes.NewBuffer(body))
+	clonedBody := bytes.NewBuffer(body)
+	return clonedBody, nil
+}
+
+func (r *ClientRetryer) cloneRequest(req *http.Request) *http.Request {
+	body, _ := r.cloneRequestBody(req)
+	clonedRequest, err := http.NewRequest(req.Method, req.URL.String(), body)
+	if err != nil {
+		return req
+	}
+	clonedRequest.Header = req.Header
+	return clonedRequest
+}
+
 func (r *ClientRetryer) RoundTrip(req *http.Request) (*http.Response, error) {
 	waitBeforeRetry := 100 * time.Millisecond
 	var res *http.Response
 	var err error
 	for i := 0; i < r.attempts; i++ {
-		res, err = r.Transport.RoundTrip(req)
+		reqCopy := r.cloneRequest(req)
+		res, err = r.Transport.RoundTrip(reqCopy)
+
 		if err != nil {
 			if os.IsTimeout(err) {
 				logger().Debug("Request timeout, retrying...", "attempt", i+1, "")
@@ -41,15 +65,18 @@ func (r *ClientRetryer) RoundTrip(req *http.Request) (*http.Response, error) {
 				waitBeforeRetry = waitBeforeRetry * 2
 				continue
 			}
+			req.Body.Close()
 			return res, err
 		}
-		if res.StatusCode > 404 && res.StatusCode < 600 {
+		if res.StatusCode >= 500 {
 			logger().Debug("Server responded with fail, retrying...", "attempt", i+1, "status code", res.StatusCode, "")
 			time.Sleep(waitBeforeRetry)
 			waitBeforeRetry = waitBeforeRetry * 2
 			continue
 		}
+		req.Body.Close()
 		return res, err
 	}
+	req.Body.Close()
 	return res, err
 }
