039b7f1f3cb2edf98907c35fd3b509f6e8e3eed7
Felipe Publio <felipe@publio.dev>
2024-05-05 10:53:03 -0300

    feature: improve debbug log view

--- mgc/cli/cmd/logger.go
+++ mgc/cli/cmd/logger.go
@@ -14,12 +14,14 @@ var logger = mgcLoggerPkg.NewLazy[osArgParser]()
 
 func newLogConfig() zap.Config {
 	zapConfig := zap.NewProductionConfig()
-	zapConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)             // it's widely used, zapfilter will default to "warn+:*"
-	zapConfig.Encoding = "console"                                     // after all, it's a CLI
-	zapConfig.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder  // INFO, DEBUG...
-	zapConfig.EncoderConfig.EncodeTime = zapcore.EpochNanosTimeEncoder // smaller yet high-resolution
-	zapConfig.EncoderConfig.CallerKey = ""                             // do not show file:line
-	zapConfig.EncoderConfig.TimeKey = ""                               // do not show timestamp by default
+	zapConfig.Level = zap.NewAtomicLevelAt(zap.DebugLevel)                 // it's widely used, zapfilter will default to "warn+:*"
+	zapConfig.Encoding = "console"                                         // after all, it's a CLI
+	zapConfig.EncoderConfig = zap.NewDevelopmentEncoderConfig()            // use the development encoder config
+	zapConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder // use colored level names
+	zapConfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder        // use ISO8601 format for timestamps
+	zapConfig.EncoderConfig.EncodeDuration = zapcore.StringDurationEncoder // use string duration encoder
+	zapConfig.EncoderConfig.CallerKey = ""                                 // do not show file:line
+	zapConfig.EncoderConfig.TimeKey = ""                                   // do not show timestamp by default
 	return zapConfig
 }
 
