b05f9620011eeca332feef47d5450067472cff61
Gefferson Ferraz <me@gefferson.com.br>
2024-05-13 16:13:32 -0300

    feat: remove old specs

--- mgc/api-specs/cmd/common.go
+++ mgc/api-specs/cmd/common.go
@@ -2,11 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -30,76 +27,76 @@ func verificarEAtualizarDiretorio(caminho string) error {
 	return err
 }
 
-func verificarERenomearArquivo(caminho string) error {
-	// Verifica se o arquivo já existe
-	_, err := os.Stat(caminho)
-	if err != nil {
-		if os.IsNotExist(err) {
-			// Arquivo não existe
-			return nil
-		}
-		// Outro erro ao verificar o arquivo
-		return err
-	}
+// func verificarERenomearArquivo(caminho string) error {
+// 	// Verifica se o arquivo já existe
+// 	_, err := os.Stat(caminho)
+// 	if err != nil {
+// 		if os.IsNotExist(err) {
+// 			// Arquivo não existe
+// 			return nil
+// 		}
+// 		// Outro erro ao verificar o arquivo
+// 		return err
+// 	}
 
-	// Obtém a data de criação do arquivo
-	info, err := os.Stat(caminho)
-	if err != nil {
-		return err
-	}
-	dataCriacao := info.ModTime()
-	dataCriacaoFormatada := dataCriacao.Format("2006-01-02_15-04-05")
+// 	// Obtém a data de criação do arquivo
+// 	info, err := os.Stat(caminho)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	dataCriacao := info.ModTime()
+// 	dataCriacaoFormatada := dataCriacao.Format("2006-01-02_15-04-05")
 
-	// Obtém o nome e a extensão do arquivo
-	nomeArquivo := filepath.Base(caminho)
-	extensao := filepath.Ext(caminho)
-	nomeArquivoSemExtensao := nomeArquivo[0 : len(nomeArquivo)-len(extensao)]
+// 	// Obtém o nome e a extensão do arquivo
+// 	nomeArquivo := filepath.Base(caminho)
+// 	extensao := filepath.Ext(caminho)
+// 	nomeArquivoSemExtensao := nomeArquivo[0 : len(nomeArquivo)-len(extensao)]
 
-	// Renomeia o arquivo para incluir a data de criação
-	novoNome := fmt.Sprintf("%s_%s%s.old", nomeArquivoSemExtensao, dataCriacaoFormatada, extensao)
-	novoCaminho := filepath.Join(filepath.Dir(caminho), novoNome)
-	err = os.Rename(caminho, novoCaminho)
-	if err != nil {
-		return err
-	}
+// 	// Renomeia o arquivo para incluir a data de criação
+// 	novoNome := fmt.Sprintf("%s_%s%s.old", nomeArquivoSemExtensao, dataCriacaoFormatada, extensao)
+// 	novoCaminho := filepath.Join(filepath.Dir(caminho), novoNome)
+// 	err = os.Rename(caminho, novoCaminho)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	fmt.Printf("Arquivo renomeado para: %s\n", novoCaminho)
-	return nil
-}
+// 	fmt.Printf("Arquivo renomeado para: %s\n", novoCaminho)
+// 	return nil
+// }
 
-func removerArquivosOld(diretorio string) error {
-	// Abre o diretório especificado
-	dir, err := os.Open(diretorio)
-	if err != nil {
-		return fmt.Errorf("erro ao abrir o diretório: %v", err)
-	}
-	defer dir.Close()
+// func removerArquivosOld(diretorio string) error {
+// 	// Abre o diretório especificado
+// 	dir, err := os.Open(diretorio)
+// 	if err != nil {
+// 		return fmt.Errorf("erro ao abrir o diretório: %v", err)
+// 	}
+// 	defer dir.Close()
 
-	// Lê o conteúdo do diretório
-	arquivos, err := dir.Readdir(-1)
-	if err != nil {
-		return fmt.Errorf("erro ao ler o conteúdo do diretório: %v", err)
-	}
+// 	// Lê o conteúdo do diretório
+// 	arquivos, err := dir.Readdir(-1)
+// 	if err != nil {
+// 		return fmt.Errorf("erro ao ler o conteúdo do diretório: %v", err)
+// 	}
 
-	// Itera sobre os arquivos do diretório
-	for _, arquivo := range arquivos {
-		// Verifica se é um arquivo com extensão ".old"
-		if !arquivo.IsDir() && filepath.Ext(arquivo.Name()) == ".old" {
-			// Monta o caminho completo do arquivo
-			caminhoArquivo := filepath.Join(diretorio, arquivo.Name())
+// 	// Itera sobre os arquivos do diretório
+// 	for _, arquivo := range arquivos {
+// 		// Verifica se é um arquivo com extensão ".old"
+// 		if !arquivo.IsDir() && filepath.Ext(arquivo.Name()) == ".old" {
+// 			// Monta o caminho completo do arquivo
+// 			caminhoArquivo := filepath.Join(diretorio, arquivo.Name())
 
-			// Remove o arquivo
-			err := os.Remove(caminhoArquivo)
-			if err != nil {
-				return fmt.Errorf("erro ao remover o arquivo %s: %v", caminhoArquivo, err)
-			}
+// 			// Remove o arquivo
+// 			err := os.Remove(caminhoArquivo)
+// 			if err != nil {
+// 				return fmt.Errorf("erro ao remover o arquivo %s: %v", caminhoArquivo, err)
+// 			}
 
-			fmt.Printf("Arquivo %s removido com sucesso.\n", caminhoArquivo)
-		}
-	}
+// 			fmt.Printf("Arquivo %s removido com sucesso.\n", caminhoArquivo)
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 func validarEndpoint(url string) bool {
 	resp, err := http.Get(url)
@@ -118,48 +115,48 @@ func validarEndpoint(url string) bool {
 	return true
 }
 
-func getAndSaveFromGitlab(url, caminhoDestino string) error {
+// func getAndSaveFromGitlab(url, caminhoDestino string) error {
 
-	destine := strings.Replace(url, "https://gitlab.luizalabs.com/open-platform/pcx/u0/-/raw/main/api_products", "", 1)
-	destine = strings.Replace(destine, "?ref_type=heads", "", -1)
-	destine = strings.Replace(destine, "/", "%2F", -1)
+// 	destine := strings.Replace(url, "https://gitlab.luizalabs.com/open-platform/pcx/u0/-/raw/main/api_products", "", 1)
+// 	destine = strings.Replace(destine, "?ref_type=heads", "", -1)
+// 	destine = strings.Replace(destine, "/", "%2F", -1)
 
-	url = "https://gitlab.luizalabs.com/api/v4/projects/7739/repository/files/api_products"
-	url += destine + "/raw?ref=main"
+// 	url = "https://gitlab.luizalabs.com/api/v4/projects/7739/repository/files/api_products"
+// 	url += destine + "/raw?ref=main"
 
-	// Faz o download do arquivo JSON
+// 	// Faz o download do arquivo JSON
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return fmt.Errorf("erro ao criar a solicitação HTTP: %v", err)
-	}
+// 	req, err := http.NewRequest("GET", url, nil)
+// 	if err != nil {
+// 		return fmt.Errorf("erro ao criar a solicitação HTTP: %v", err)
+// 	}
 
-	// Define o cabeçalho de autenticação com o token fornecido
+// 	// Define o cabeçalho de autenticação com o token fornecido
 
-	req.Header.Set("PRIVATE-TOKEN", os.Getenv("TOKEN_GITLAB"))
+// 	req.Header.Set("PRIVATE-TOKEN", os.Getenv("TOKEN_GITLAB"))
 
-	// Envia a solicitação HTTP
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("erro ao fazer a solicitação HTTP: %v", err)
-	}
-	defer resp.Body.Close()
+// 	// Envia a solicitação HTTP
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		return fmt.Errorf("erro ao fazer a solicitação HTTP: %v", err)
+// 	}
+// 	defer resp.Body.Close()
 
-	// Lê o corpo da resposta
-	dados, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("erro ao ler o corpo da resposta: %v", err)
-	}
+// 	// Lê o corpo da resposta
+// 	dados, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return fmt.Errorf("erro ao ler o corpo da resposta: %v", err)
+// 	}
 
-	// Grava os dados no arquivo local
-	err = os.WriteFile(caminhoDestino, dados, 0644)
-	if err != nil {
-		return fmt.Errorf("erro ao gravar os dados no arquivo: %v", err)
-	}
+// 	// Grava os dados no arquivo local
+// 	err = os.WriteFile(caminhoDestino, dados, 0644)
+// 	if err != nil {
+// 		return fmt.Errorf("erro ao gravar os dados no arquivo: %v", err)
+// 	}
 
-	fmt.Println("Arquivo JSON baixado e salvo com sucesso.")
-	return nil
-}
+// 	fmt.Println("Arquivo JSON baixado e salvo com sucesso.")
+// 	return nil
+// }
 
 // func getAndSaveFile(url, caminhoDestino string) error {
 // 	// Faz o download do arquivo JSON
