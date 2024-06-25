04d201bcd97b8bd5e95806805d17abde0f5028f3
Gefferson Ferraz <me@gefferson.com.br>
2024-06-25 18:04:35 -0300

    feat: adding new resource tf

--- mgc/terraform-provider-mgc-sdk/internal/provider/mgc_common.go
+++
@@ -1,8 +0,0 @@
-package provider
-
-import "github.com/hashicorp/terraform-plugin-framework/types"
-
-type genericIDNameModel struct {
-	ID   types.String `tfsdk:"id"`
-	Name types.String `tfsdk:"name"`
-}
