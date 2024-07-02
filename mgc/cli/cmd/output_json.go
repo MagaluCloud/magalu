bc29e432c7e47a64c676b3edf760f1f35a1f2e25
Felipe Publio <felipe@publio.dev>
2024-07-01 21:38:28 -0300

    feat: json outout with color

--- mgc/cli/cmd/output_json.go
+++ mgc/cli/cmd/output_json.go
@@ -3,17 +3,44 @@ package cmd
 import (
 	"encoding/json"
 	"os"
+
+	"github.com/mattn/go-colorable"
+	jsonColor "github.com/neilotoole/jsoncolor"
 )
 
 type jsonOutputFormatter struct{}
 
-func (*jsonOutputFormatter) Format(value any, options string) error {
-	enc := json.NewEncoder(os.Stdout)
+func (*jsonOutputFormatter) Format(value any, options string, isRaw bool) error {
+	if isRaw {
+		enc := json.NewEncoder(os.Stdout)
+		if options == "compact" {
+			enc.SetIndent("", "")
+		} else {
+			enc.SetIndent("", " ")
+		}
+		return enc.Encode(value)
+	}
+	out := colorable.NewColorable(os.Stdout) // needed for Windows
+	enc := jsonColor.NewEncoder(out)
+
 	if options == "compact" {
 		enc.SetIndent("", "")
 	} else {
 		enc.SetIndent("", " ")
 	}
+
+	clrs := jsonColor.DefaultColors()
+    // Update colors based on provided settings
+    clrs.Bool = jsonColor.Color("\x1b[95m") // FgHiMagenta for boolean
+    clrs.Number = jsonColor.Color("\x1b[95m") // FgHiMagenta for number
+    clrs.Key = jsonColor.Color("\x1b[96m") // FgHiCyan for map keys
+    // Assuming Anchor and Alias are not directly supported by jsoncolor, so we'll skip these
+    clrs.String = jsonColor.Color("\x1b[92m") // FgHiGreen for strings
+    // Reset color is not needed here as jsoncolor handles it
+
+
+	enc.SetColors(clrs)
+
 	return enc.Encode(value)
 }
 
