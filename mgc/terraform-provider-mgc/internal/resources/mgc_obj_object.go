02ec03e806208efb13ee5b1f5b378e22e49ed311
Gefferson Ferraz <me@gefferson.com.br>
2024-08-21 16:36:20 -0300

    feat: tf add obj resource with datasource

--- mgc/terraform-provider-mgc/internal/resources/mgc_obj_object.go
+++ mgc/terraform-provider-mgc/internal/resources/mgc_obj_object.go
@@ -1,9 +1,1 @@
 package resources
-
-import "github.com/hashicorp/terraform-plugin-framework/types"
-
-type ObjectStorageObject struct {
-	Bucket       types.String `tfsdk:"bucket"`
-	Source       types.String `tfsdk:"source"`
-	StorageClass types.String `tfsdk:"storage_class"`
-}
