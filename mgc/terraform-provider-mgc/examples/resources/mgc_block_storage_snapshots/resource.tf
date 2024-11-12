23566676a4a06bd345600dbfcc63d0f276b830e4
Gefferson Ferraz <me@gefferson.com.br>
2024-11-12 15:06:05 -0300

    fix: tf - update block-storage

--- mgc/terraform-provider-mgc/examples/resources/mgc_block_storage_snapshots/resource.tf
+++ mgc/terraform-provider-mgc/examples/resources/mgc_block_storage_snapshots/resource.tf
@@ -1,6 +1,8 @@
 resource "mgc_block_storage_snapshots" "snapshot_example" {
   description = "example of description"
   name        = "exemplo snapshot name"
+  snapshot_source_id = mgc_block_storage_snapshots.other_snapshot.id
+  type        = "instant"
   volume = {
     id = mgc_block_storage_volumes.example_volume.id
   }
--- mgc/terraform-provider-mgc/examples/resources/mgc_block_storage_volumes/resource.tf
+++ mgc/terraform-provider-mgc/examples/resources/mgc_block_storage_volumes/resource.tf
@@ -1,5 +1,6 @@
 resource "mgc_block_storage_volumes" "example_volume" {
   name = "example-volume"
+  availability_zone = "br-se1-a"
   size = 10
   type = {
     name = "cloud_nvme"
