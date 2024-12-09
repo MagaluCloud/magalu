1a2a0cb4df14a0fdcd08d4aad0a3c71ee82fdecf
Felipe Publio <felipe@publio.dev>
2024-12-09 13:25:31 -0300

    feat: terraform database instances implemented

--- mgc/terraform-provider-mgc/examples/resources/mgc_dbaas_instances/resource.tf
+++ mgc/terraform-provider-mgc/examples/resources/mgc_dbaas_instances/resource.tf
@@ -1,10 +1,10 @@
 resource "mgc_dbaas_instances" "dbaas_instances" {
-  name             = "my-database-instance"
-  user             = "db_user"
-  password         = "secure_password123"
-  engine_id        = "063f3994-b6c2-4c37-96c9-bab8d82d36f7" #mysql 8.0 - please check the available version
-  instance_type_id = "c460d5c1-883d-4fea-afc3-1a208e982084" #cloud-dbaas-bs1.medium - please check the available engine
-  volume = {
-    size = 50 # Size in GiB
-  }
+  id    =  computed
+  name           = "my-database-instance"
+  user           = "db_user"
+  password       = "secure_password123"
+  engine_name    = "mysql"
+  engine_version = "8.0"
+  instance_type  = "mgc.db.tiny"
+  volume_size    = 10
 }
