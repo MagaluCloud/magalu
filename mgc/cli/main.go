889c94368ff22b523d20ffd2f84a14d6b8ddb9a0
Gefferson Ferraz <me@gefferson.com.br>
2025-03-14 14:15:37 -0300

    feat: update kin

--- mgc/cli/main.go
+++ mgc/cli/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"strings"
 
 	"github.com/MagaluCloud/magalu/mgc/cli/cmd"
 	mgcSdk "github.com/MagaluCloud/magalu/mgc/sdk"
 )
 
 func main() {
-	// defer panicRecover()
+	defer panicRecover()
 	mgcSdk.SetUserAgent("MgcCLI")
 
 	err := cmd.Execute()
@@ -19,15 +21,15 @@ func main() {
 	}
 }
 
-// func panicRecover() {
-// 	err := recover()
-// 	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "\n😔 Oops! Something went wrong.\nPlease help us improve by sending the error report to: https://help.github.com/MagaluCloud/magalu/mgc/hc/pt-br/requests/new\n\n  Version: %s\n  SO: %s / %s\n  Args: %s\n  Error: %s\n\nThank you for your cooperation!\n\n",
-// 			mgcSdk.Version,
-// 			runtime.GOOS,
-// 			runtime.GOARCH,
-// 			strings.Join(os.Args, " "),
-// 			err)
-// 		os.Exit(1)
-// 	}
-// }
+func panicRecover() {
+	err := recover()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\n😔 Oops! Something went wrong.\nPlease help us improve by sending the error report to: https://help.github.com/MagaluCloud/magalu/mgc/hc/pt-br/requests/new\n\n  Version: %s\n  SO: %s / %s\n  Args: %s\n  Error: %s\n\nThank you for your cooperation!\n\n",
+			mgcSdk.Version,
+			runtime.GOOS,
+			runtime.GOARCH,
+			strings.Join(os.Args, " "),
+			err)
+		os.Exit(1)
+	}
+}
