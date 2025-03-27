67442f5e6b0c784c81f1ea5ebdee28117b575776
Gefferson Ferraz <me@gefferson.com.br>
2025-03-27 10:33:04 -0300

    chore: remove version.txt

--- mgc/sdk/version.go
+++ mgc/sdk/version.go
@@ -2,17 +2,11 @@ package sdk
 
 import (
 	_ "embed"
-	"fmt"
-	"os"
 	"strings"
 )
 
-//go:embed version.txt
-var rawVersion string
+var RawVersion string
 
 var version string = func() string {
-	if vv := os.Getenv("VERSION"); vv != "" {
-		return fmt.Sprintf("v%s", strings.TrimPrefix(vv, "v"))
-	}
-	return strings.Trim(rawVersion, " \t\n\r")
+	return strings.Trim(RawVersion, " \t\n\r")
 }()
