f79a3a1ff69324335c38aecf4220f7b8d0d247c5
Daniel Debonzi <daniel.debonzi@luizalabs.com>
2025-12-09 17:17:32 -0300

    Add support for mgc federations running on local machine.

--- scripts/add_all_specs.sh
+++ scripts/add_all_specs.sh
@@ -27,6 +27,8 @@ $BASEDIR/add_specs.sh load-balancer load-balancer specs/lbaas.openapi.yaml https
 
 $BASEDIR/add_specs_without_region.sh iam iam specs/iam.jaxyendy.openapi.json https://iam.jaxyendy.com/openapi.json
 
+$BASEDIR/add_specs_without_region.sh federation federation specs/federation.local.openapi.json http://127.0.0.1:8000/openapi.json
+
 make oapi-index-gen
 # EXAMPLE
 # $BASEDIR/SCRIPT.sh NOME_NO_MENU URL_PATH LOCAL_DA_SPEC SPEC_UID
