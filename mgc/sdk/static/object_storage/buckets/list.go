658a2e7ef1231094e6538406af75c50d4b64f5ce
Gefferson Ferraz <me@gefferson.com.br>
2025-12-17 10:42:50 -0300

    fix: bucket size

--- mgc/sdk/static/object_storage/buckets/list.go
+++ mgc/sdk/static/object_storage/buckets/list.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/MagaluCloud/magalu/mgc/core"
 	"github.com/MagaluCloud/magalu/mgc/core/utils"
@@ -12,9 +11,16 @@ import (
 )
 
 type BucketResponse struct {
-	CreationDate string `xml:"CreationDate"`
-	Name         string `xml:"Name"`
-	BucketSize   string `xml:"BucketSize"`
+	CreationDate string       `xml:"CreationDate"`
+	Name         string       `xml:"Name"`
+	Size         SizeResponse `xml:"Size"`
+}
+
+type SizeResponse struct {
+	Standard    string `xml:"Standard"`
+	ColdInstant string `xml:"ColdInstant"`
+	Versions    string `xml:"Versions"`
+	UpdatedAt   string `xml:"UpdatedAt"`
 }
 
 type ListResponse struct {
@@ -22,8 +28,35 @@ type ListResponse struct {
 	Owner   *common.Owner     `xml:"Owner"`
 }
 
+type Bucket struct {
+	CreationDate string `xml:"CreationDate"`
+	Name         string `xml:"Name"`
+	Size         Size   `xml:"Size"`
+}
+
+type Size struct {
+	Standard    float64 `xml:"Standard"`
+	ColdInstant float64 `xml:"ColdInstant"`
+	Versions    float64 `xml:"Versions"`
+	UpdatedAt   string  `xml:"UpdatedAt"`
+}
+
+type APIReturn struct {
+	Buckets []*Bucket     `xml:"Buckets>Bucket"`
+	Owner   *common.Owner `xml:"Owner"`
+}
+
 func newListRequest(ctx context.Context, cfg common.Config) (*http.Request, error) {
-	return http.NewRequestWithContext(ctx, http.MethodGet, string(common.BuildHost(cfg)), nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, string(common.BuildHost(cfg)), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	q.Add("size", "")
+	req.URL.RawQuery = q.Encode()
+
+	return req, nil
 }
 
 var getList = utils.NewLazyLoader[core.Executor](func() core.Executor {
@@ -31,7 +64,6 @@ var getList = utils.NewLazyLoader[core.Executor](func() core.Executor {
 		core.DescriptorSpec{
 			Name:        "list",
 			Description: "List all existing Buckets",
-			// Scopes:      core.Scopes{"object-storage.read"},
 		},
 		list,
 	)
@@ -41,47 +73,53 @@ var getList = utils.NewLazyLoader[core.Executor](func() core.Executor {
 	return exec
 })
 
-func list(ctx context.Context, _ struct{}, cfg common.Config) (result ListResponse, err error) {
+func list(ctx context.Context, _ struct{}, cfg common.Config) (*ListResponse, error) {
 	req, err := newListRequest(ctx, cfg)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	resp, err := common.SendRequest(ctx, req, cfg)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	result, err = common.UnwrapResponse[ListResponse](resp, req)
+	response, err := common.UnwrapResponse[APIReturn](resp, req)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	for _, bucket := range result.Buckets {
-		size, err := strconv.ParseInt(bucket.BucketSize, 10, 64)
-		if err != nil {
-			bucket.BucketSize = "-"
-		} else {
-			bucket.BucketSize = FormatSize(size)
-		}
+	var result ListResponse
+	result.Owner = response.Owner
+	for _, bucket := range response.Buckets {
+		result.Buckets = append(result.Buckets, &BucketResponse{
+			CreationDate: bucket.CreationDate,
+			Name:         bucket.Name,
+			Size: SizeResponse{
+				Standard:    FormatSize(bucket.Size.Standard),
+				ColdInstant: FormatSize(bucket.Size.ColdInstant),
+				Versions:    FormatSize(bucket.Size.Versions),
+				UpdatedAt:   bucket.Size.UpdatedAt,
+			},
+		})
 	}
 
-	return
+	return &result, nil
 }
 
-func FormatSize(size int64) string {
+func FormatSize(size float64) string {
 	const unit = 1024
 	if size < unit {
-		return fmt.Sprintf("%dB", size)
+		return fmt.Sprintf("%.1fB", size)
 	}
 
 	suffixes := []string{"KiB", "MiB", "GiB", "TiB", "PiB", "EiB"}
-	i := 0
+	i := -1
 	sizeFloat := float64(size)
 
 	for sizeFloat >= unit && i < len(suffixes)-1 {
-		sizeFloat /= unit
 		i++
+		sizeFloat /= unit
 	}
 
 	return fmt.Sprintf("%.1f %s", sizeFloat, suffixes[i])
